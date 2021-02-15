bdd1fe03e27c8ddbbeb74b3ae3ffce6ccade05dc
iakovts <iakovts@yahoo.com>
2021-02-15 16:41:30 +0200

    [EXE1] Refactor python compare script

--- exe1/compare_res.py
+++ exe1/compare_res.py
@@ -1,14 +1,22 @@
 import glob
+import argparse
 
 import numpy as np
 
-files = glob.glob("res*_threads.txt")
 
-data = [np.fromfile(i, sep=" ") for i in files]
+def argument_parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--files", "-f", nargs="+", required=False)
+    return parser.parse_args()
 
-comp = (np.diff(np.vstack(data).reshape(len(data), -1), axis=0)==0).all()
 
-if comp:
-    print("Output from all threads {1..4} is the same")
-else:
-    print("Outputs are different for different number of threads")
+if __name__ == "__main__":
+    args = argument_parse()
+    if not args.files:
+        files = glob.glob("res*_threads.txt")
+    data = [np.fromfile(i, sep=" ") for i in files]
+    comp = (np.diff(np.vstack(data).reshape(len(data), -1), axis=0)==0).all()
+    if comp:
+        print("Results are the same for all test cases")
+    else:
+        print("Results are different for different number of threads")
